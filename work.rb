d5d27561f54fbea6d882b085d4dc1ac5735ae615
lv.jiiiiu <lv_jiiiiu@yahoo.co.jp>
2021-03-16 04:35:13 +0000

    4.5.5章

---
+++ work.rb
@@ -0,0 +1,22 @@
+numbers = [1, 2, 3, 4, 5]
+new_numbers = numbers.map do |n|
+  n*10
+end 
+
+even_numbers = numbers.select {|n| n.even? }
+even_numbers = numbers.reject {|n| n.even? }
+
+sum = numbers.inject {|result, n| result + n}
+
+def change(age)
+  case age
+  when 1..5
+    0
+  when 5..10
+    300
+  else 
+    1000
+  end 
+end 
+ 
+  (1..5).to
