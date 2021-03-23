8c1b716b67265c8c5010532b487b5d014e2f0ca1
lv.jiiiiu <lv_jiiiiu@yahoo.co.jp>
2021-03-23 01:07:08 +0000

    改札機プログラム完成

--- lib/gate.rb
+++ lib/gate.rb
@@ -1,15 +1,29 @@
 class Gate
+  STATIONS = [:umeda, :juso, :mikuni]
+  FARES = [150, 190]
+
+
   # initializeメソッドを定義して引数として駅の名前を受け取れるようにする
   def initialize(name)
-    @name = name 
+    @name = name
   end
-  
+
   def enter(ticket)
     ticket.stamp(@name)
-    
-  end 
-  
-  def exit(ticket)
-    
+
   end
+
+  def exit(ticket)
+    fare = calc_fare(ticket)
+    fare <= ticket.fare
+
+  end
+
+  def calc_fare(ticket)
+    from = STATIONS.index(ticket.stamped_at)
+    to = STATIONS.index(@name)
+    distance = to - from
+    FARES[distance - 1]
+  end
+
 end
