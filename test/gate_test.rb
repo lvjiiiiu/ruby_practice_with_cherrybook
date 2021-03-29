0f0d180bc38bf51717908d368475e4c4c50b74d9
lv.jiiiiu <lv_jiiiiu@yahoo.co.jp>
2021-03-29 11:15:15 +0000

    ダックタイピング

--- test/gate_test.rb
+++ test/gate_test.rb
@@ -107,3 +107,30 @@ display_name(user)
 product = Product.new
 disyplay_name(product)
 
+class Product 
+  def initialize(name, price)
+    @name = name
+    @price = price
+  end
+  
+  def display_text
+    stock = stock? ? "あり":"なし"
+    "商品名:#{@name} 価格:#{@price}円 在庫:#{stock}"
+  end 
+  
+  def stock?
+    raise "Must implement stock? in subclass."
+  end 
+end
+
+class DVD < Product
+  def stock?
+    true
+  end 
+end 
+
+product = Product.new("A great film", 1000)
+product.display_text
+
+dvd = DVD.new("An awsome film", 3000)
+dvd.display_text
