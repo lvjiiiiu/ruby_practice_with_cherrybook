fba01f5292bb2b9091f04f6bf334b31ba1163383
lv.jiiiiu <lv_jiiiiu@yahoo.co.jp>
2021-03-30 21:59:59 +0000

    8.6終了

--- work.rb
+++ work.rb
@@ -108,3 +108,22 @@ s = "abc"
 s.log("Hello.") #=>エラー
 s.extend(Loggable) #=>文字列sにLoggableモジュールのメソッドを特異メソッドとしてミックスインする。
 s.log("Hello.") #=>[LOG]Hello.
+
+
+module Baseball
+
+	class Second
+		def initialize(player, uniform_number)
+			@player = player
+			@uniform_number = uniform_number
+		end
+	end
+end
+
+module Clock
+	class Second
+		def initialize(digits)
+			@digits = digits
+		end
+	end
+end
