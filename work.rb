5de08ec8c4e348d91a38e3521b51849d0efae1ef
lv.jiiiiu <lv_jiiiiu@yahoo.co.jp>
2021-03-30 21:11:07 +0000

    8.5.4

--- work.rb
+++ work.rb
@@ -73,3 +73,25 @@ end
 
 product = Product.new("A free movie")
 product.price 
+
+class Tempo
+  include Comparable
+  
+  attr_reader :bpm
+  
+  def initialize(bpm)
+    @bpm = bpm
+  end
+  
+  def <=>(other)
+    if other.is_a?(Tempo)
+      bpm <=> other.bpm
+    else
+      nil
+    end
+  end
+  
+  def inspect
+    "#{bpm}bpm"
+  end 
+end 
