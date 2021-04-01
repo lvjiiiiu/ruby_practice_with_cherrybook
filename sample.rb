9e90de6ddc88647b9247e8be85eabd0467cc2ccd
lv.jiiiiu <lv_jiiiiu@yahoo.co.jp>
2021-04-01 00:04:06 +0000

    9.2

--- sample.rb
+++ sample.rb
@@ -1,15 +1,51 @@
-puts "Start"
-module Greeter
-def hello
-  "hello"
-end
-end
+# puts "Start"
+# module Greeter
+# def hello
+#   "hello"
+# end
+# end
 
-begin
-  greeter = Greeter.new
-rescue
-  puts "例外が発生したがこのまま続行する"
-end
+# begin
+#   greeter = Greeter.new
+# rescue
+#   puts "例外が発生したがこのまま続行する"
+# end
   
-  puts "End."
+#   puts "End."
  
+ 
+ def method_1
+   puts "method_1 start."
+   begin 
+     method_2
+   rescue
+     puts "例外が発生しました"
+   end
+   puts "method_1 end."
+ end
+ 
+ def method_2
+   puts "method_2 start."
+   method_3
+   puts "method_2 end."
+   
+ end
+ 
+ def method_3
+   puts "method_3 start."
+   
+   1 / 0
+   puts "method_3 end."
+ end
+ 
+ method_1
+ 
+begin
+1 / 0
+rescue => e
+	puts "エラークラス: #{e.class}"
+	puts "エラーメッセージ: #{e.message}"
+	puts "バックトレース____"
+	puts e.backtrace
+	puts "____"
+end
