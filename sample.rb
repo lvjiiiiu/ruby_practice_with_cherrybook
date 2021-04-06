0c090ebc5217c22cd76dea623d37824fa90af9c5
lv.jiiiiu <lv_jiiiiu@yahoo.co.jp>
2021-04-06 11:17:47 +0000

    Procオブジェクト

--- sample.rb
+++ sample.rb
@@ -130,25 +130,29 @@
 #   text.upcase
 # end 
  
- def greeting(&block)
-   puts "おはよう"
-   text = 
-   if block.arity == 1
-     yield "こんにちは"
-   elsif block.arity == 2
-     yield "こんに", "ちは"
-   end 
+# def greeting(&block)
+#   puts "おはよう"
+#   text = 
+#   if block.arity == 1
+#     yield "こんにちは"
+#   elsif block.arity == 2
+#     yield "こんに", "ちは"
+#   end 
 
-   puts text 
-   puts "こんばんは"
- end
+#   puts text 
+#   puts "こんばんは"
+# end
  
- greeting do |text|
-   text * 2
- end 
+# greeting do |text|
+#   text * 2
+# end 
  
- greeting do |text_1, text_2|
-   text_1 * 2 + text_2 *2 
- end 
+# greeting do |text_1, text_2|
+#   text_1 * 2 + text_2 *2 
+# end 
+
  
- 
+add_proc = Proc.new {|a = 0, b = 0| a + b }
+p add_proc.call
+p add_proc.call(10)
+puts add_proc.call(10, 20)
