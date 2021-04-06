37c9f4134acf245ec63e3cc64faba8a828b39511
lv.jiiiiu <lv_jiiiiu@yahoo.co.jp>
2021-04-06 11:01:01 +0000

    10.2

--- sample.rb
+++ sample.rb
@@ -106,27 +106,49 @@
  
 #  currency_of(:italy) #=> NoCountryError: 無名な国名です。italy
 
- def greeting_ja(&block)
-   texts = ["おはよう", "こんにちは", "こんばんは"]
-   greeting_common(texts, &block)
+# def greeting_ja(&block)
+#   texts = ["おはよう", "こんにちは", "こんばんは"]
+#   greeting_common(texts, &block)
+# end
+ 
+# def greeting_en(&block)
+#   texts = ["good morinig", "hello", "good evening"]
+#   greeting_common(texts, &block)
+# end 
+ 
+# def greeting_common(texts, &block)
+#   puts texts[0]
+#   puts block.call(texts[1])
+#   puts texts[2]
+# end
+ 
+# greeting_ja do |text|
+#   text * 2
+# end 
+ 
+# greeting_en do |text|
+#   text.upcase
+# end 
+ 
+ def greeting(&block)
+   puts "おはよう"
+   text = 
+   if block.arity == 1
+     yield "こんにちは"
+   elsif block.arity == 2
+     yield "こんに", "ちは"
+   end 
+
+   puts text 
+   puts "こんばんは"
  end
  
- def greeting_en(&block)
-   texts = ["good morinig", "hello", "good evening"]
-   greeting_common(texts, &block)
- end 
- 
- def greeting_common(texts, &block)
-   puts texts[0]
-   puts block.call(texts[1])
-   puts texts[2]
- end
- 
- greeting_ja do |text|
+ greeting do |text|
    text * 2
  end 
  
- greeting_en do |text|
-   text.upcase
+ greeting do |text_1, text_2|
+   text_1 * 2 + text_2 *2 
  end 
+ 
  
