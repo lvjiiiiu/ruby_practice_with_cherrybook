b9794cd7d18f4006ef25d809fa5245dd4dc666f9
lv.jiiiiu <lv_jiiiiu@yahoo.co.jp>
2021-04-06 12:09:14 +0000

    テスト作成

---
+++ test/effects_test.rb
@@ -0,0 +1,10 @@
+ require "minitest/autorun"
+ require "./lib/effects"
+ 
+ class EffectsTest < Minitest::Test
+   def test_reverse
+     assert EffectsTest
+   end
+ end 
+ 
+ 
---
+++ test/word_synth_test.rb
@@ -0,0 +1,10 @@
+require "minitest/autorun"
+require "./lib/word_synth"
+require "./lib/effects"
+
+class WordSynthTest < Minitest::Test
+  def test_play
+    assert WordSynthTest
+    assert Effects
+  end
+end 
