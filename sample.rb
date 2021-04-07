1478c8031f0415c6285a33b2f090846a8e5aea4b
lv.jiiiiu <lv_jiiiiu@yahoo.co.jp>
2021-04-07 00:12:58 +0000

    10.5

--- sample.rb
+++ sample.rb
@@ -169,13 +169,28 @@
 # greeting(repeat_proc)
 
 
-def greeting(proc_1, proc_2, proc_3)
-  puts proc_1.call("おはよう")
-  puts proc_2.call("こんにちは")
-  puts proc_3.call("こんばんは")
+# def greeting(proc_1, proc_2, proc_3)
+#   puts proc_1.call("おはよう")
+#   puts proc_2.call("こんにちは")
+#   puts proc_3.call("こんばんは")
+# end 
+
+# shuffle_proc = Proc.new {|text|text.chars.shuffle.join}
+# repeat_proc = Proc.new {|text|text * 2}
+# question_proc = Proc.new {|text| "#{text}?"}
+# greeting(shuffle_proc, repeat_proc, question_proc)
+
+def proc_return
+  f = Proc.new {|n| return n * 10}
+  ret = [1, 2, 3].map(&f)
+  "ret: #{ret}"
 end 
 
-shuffle_proc = Proc.new {|text|text.chars.shuffle.join}
-repeat_proc = Proc.new {|text|text * 2}
-question_proc = Proc.new {|text| "#{text}?"}
-greeting(shuffle_proc, repeat_proc, question_proc)
+def proc_break
+  f = ->(n) { return n * 10}
+  ret = [1, 2, 3].map(&f)
+  "ret: #{ret}"
+end 
+
+puts proc_return
+puts proc_break
